e76dce5d9c8ab0c0caaa8af1cbfc7acbff0196c5
kartik-270 <kartik2310179@akgec.ac.in>
2026-02-05 23:42:01 +0530

    fix port issue
    fix port issue

--- app.py
+++ app.py
@@ -81,4 +81,4 @@ def serve_uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, port=5000)
+    socketio.run(app, debug=True, port=int(os.environ.get("PORT", 5000)))
